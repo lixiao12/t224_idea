2fc5a02a3eab3d1b73099932426598008f4899d9
lixiao12 <2949424758@qq.com>
2019-10-17 09:53:37 +0800

    git send local repository

---
+++ gitssm/src/com/zking/git/Demo.java
@@ -0,0 +1,13 @@
+package com.zking.git;
+
+/**
+ * @author 小李飞刀
+ * @site www.javaxl.com
+ * @company
+ * @create  2019-10-17 9:50
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
